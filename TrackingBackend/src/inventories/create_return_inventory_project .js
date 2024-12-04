744cbb05617542799832e27f4d4c96050d90fcdf
Tun Lin Naing 8888 <144895660+tun-lin-naing-88@users.noreply.github.com>
2024-12-05 02:33:00 +0700

    change table for transaction

--- TrackingBackend/src/inventories/create_return_inventory_project .js
+++ TrackingBackend/src/inventories/create_return_inventory_project .js
@@ -1,69 +1,44 @@
 const express = require("express");
-const poolQuery = require("../../misc/poolQuery");
+const { createProjectInventoryTransaction } = require("../projects/projects.js")
+const { InventoryTransactionTypeEnum } = require("../utils/enums.js");
+const _ = require('lodash');
 
 const createReturnInventoryProjectRouter = express.Router();
+const logTransaction = require("../transactions/logTransaction")
 
-createReturnInventoryProjectRouter.post("/", async (req, res) => {
-    const {
-        project_inventory_id, // Required field
-        total_qty, // Required field
-        received_qty, // Required field
-        return_qty, // Required field
-        return_date, // Required field
-        receive_admin_name, // Required field
-        return_admin_name, // Required field
-        remark, // Optional field
-        description // Optional field
-    } = req.body.input;
+createReturnInventoryProjectRouter.post('/', async (req, res) => {
+  const { project_id, inventory_id, total_qty, requested_at, description } = req.body.input;  // extract inputs
 
-    // Required fields check
-    const requiredFields = [project_inventory_id, total_qty, received_qty, return_qty, return_date, receive_admin_name, return_admin_name];
-    for (let field of requiredFields) {
-        if (typeof field === "undefined") {
-            res.json({ success: false, message: "All required fields must be provided." });
-            return;
-        }
-    }
+  let request_admin = req.idFromToken;
 
-    try {
-        const result = await createReturnInventoryProject({
-            project_inventory_id, total_qty, received_qty, return_qty, return_date, receive_admin_name, return_admin_name, remark, description
+  try {
+    // Validate required fields
+    const requiredFields = [project_id, inventory_id, total_qty, requested_at];
+    for (const [fieldName, fieldValue] of Object.entries(requiredFields)) {
+      if (_.isEmpty(fieldValue?.trim())) {
+        return res.json({
+          success: false,
+          message: `Missing required field: ${fieldName}`
         });
-        res.json({ success: true, message: "Return inventory project created successfully", id: result.id, created_at: result.created_at });
-    } catch (error) {
-        res.json({ success: false, message: `${error.message}` });
+      }
     }
+
+    const result = await createProjectInventoryTransaction(project_id, inventory_id, total_qty, request_admin, requested_at, InventoryTransactionTypeEnum.RETURNED, description);
+
+    logTransaction(TransactionTypeEnum.INVENTORY, TransactionStatusEnum.RETURNED, `Inventory : ${result.scit_control_number}  to Project: ${result.project_name}`, request_admin);
+
+    res.json({
+      id: result.id,
+      created_at: result.created_at,
+      success: true,
+      message: "Assign inventory to project return successfully"
+    });
+  } catch (error) {
+    res.json({
+      success: false,
+      message: `${error.message}`
+    });
+  }
 });
-
-const createReturnInventoryProject = async (returnInventoryData) => {
-    const {
-        project_inventory_id, total_qty, received_qty, return_qty, return_date, receive_admin_name, return_admin_name, remark, description
-    } = returnInventoryData;
-
-    // SQL query to insert a return inventory entry
-    const query = `
-        INSERT INTO return_inventory_projects (
-            project_inventory_id,
-            total_qty,
-            received_qty,
-            return_qty,
-            return_date,
-            receive_admin_name,
-            return_admin_name,
-            remark,
-            description,
-            created_at
-        )
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, NOW())
-        RETURNING id, created_at;
-    `;
-
-    const values = [
-        project_inventory_id, total_qty, received_qty, return_qty, return_date, receive_admin_name, return_admin_name, remark, description
-    ];
-
-    const result = await poolQuery(query, values);
-    return result.rows[0]; // Return the inserted record's ID and created_at timestamp
-};
 
 module.exports = createReturnInventoryProjectRouter;
--- TrackingBackend/src/projects/assigned_inventory_to_project.js
+++ TrackingBackend/src/projects/assigned_inventory_to_project.js
@@ -1,27 +1,37 @@
-const poolQuery = require("../../misc/poolQuery.js");
 const express = require("express");
-const { assignedInventoryToProject } = require("./projects.js")
+const { createProjectInventoryTransaction } = require("./projects.js")
+const _ = require('lodash');
 
 const assignedInventoryToProjectRouter = express.Router();
 const logTransaction = require("../transactions/logTransaction")
-
-const { ProjectStatusEnum, AssignedProjectStatusEnum,
-  TaskStatusEnum, AssignedTaskStatusEnum,
-  ProjectInventoryStatusEnum, TaskInventoryStatusEnum,
-  TransactionTypeEnum, TransactionStatusEnum } = require("../../src/utils/enums")
+const { TransactionTypeEnum, TransactionStatusEnum, InventoryTransactionTypeEnum } = require("../../src/utils/enums")
 
 assignedInventoryToProjectRouter.post('/', async (req, res) => {
-  const { project_id, inventory_id, total_qty } = req.body.input;  // extract inputs
-  let created_by = req.idFromToken;
+  const { project_id, inventory_id, total_qty, requested_at, description } = req.body.input;  // extract inputs
   
+  let request_admin = req.idFromToken;
+
   try {
-    const result = await assignedInventoryToProject(project_id, inventory_id, total_qty, created_by);
-    
-    logTransaction(TransactionTypeEnum.PROJECT, TransactionStatusEnum.ADD, `Inventory : ${result.scit_control_number}  to Project: ${result.project_name}`, created_by);
+    // Validate required fields
+    const requiredFields = [project_id, inventory_id, total_qty, requested_at];
+    for (const [fieldName, fieldValue] of Object.entries(requiredFields)) {
+      if (_.isEmpty(fieldValue?.trim())) {
+        return res.json({
+          success: false,
+          message: `Missing required field: ${fieldName}`
+        });
+      }
+    }
+
+    const result = await createProjectInventoryTransaction(project_id, inventory_id, total_qty, request_admin, requested_at, InventoryTransactionTypeEnum.REQUEST, description);
+
+    logTransaction(TransactionTypeEnum.INVENTORY, TransactionStatusEnum.REQUEST, `Inventory : ${result.scit_control_number}  to Project: ${result.project_name}`, request_admin);
 
     res.json({
+      id: result.id,
+      created_at : result.created_at,
       success: true,
-      message: "Assigen inventory to project successfully"
+      message: "Assign inventory to project request successfully"
     });
   } catch (error) {
     res.json({
